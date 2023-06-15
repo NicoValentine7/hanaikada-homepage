513223c46099ca48344c65d9bb725f3dab69a26e
Nico <50232024+NicoValentine7@users.noreply.github.com>
2023-06-15 15:43:31 +0900

    initialize commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ResponsiveAppBar from './components/custom_app_bar';
 
 function App() {
   return (
     <div className="App">
+      <ResponsiveAppBar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+              <section id="About Us">
+        <h2>会社概要</h2>
+        <p>会社名 株式会社花筏</p>
+        <p>事業内容 ソフトウェアエンジニアリング</p>
+        </section>
       </header>
+
     </div>
   );
 }
